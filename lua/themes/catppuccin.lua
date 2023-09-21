8bfb824b5bbde645fc600be4a462168817224d91
weston <lexianbox@163.com>
2023-09-22 00:40:12 +0800

    first commit

--- lua/themes/catppuccin.lua
+++ lua/themes/catppuccin.lua
@@ -17,12 +17,12 @@ require("catppuccin").setup({
     no_underline = false, -- Force no underline
     styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
         comments = { "italic" }, -- Change the style of comments
-        conditionals = { "italic" },
+        conditionals = {},
         loops = {},
-        functions = {"italic"},
+        functions = {},
         keywords = {},
         strings = {},
-        variables = {"italic"},
+        variables = {},
         numbers = {},
         booleans = {},
         properties = {},
